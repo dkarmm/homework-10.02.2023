c54bd908d40ccd048980f0409334139dc313468f
dkarmm <dartikarmeev@mail.ru>
2023-02-10 18:40:18 +0500

    example008-end.

---
+++ example008/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число 
+//(N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int first_digit = 1;
+while (first_digit <= number)
+{
+    if (first_digit % 2 == 0)
+    {
+        Console.Write(first_digit + " ");
+    }
+    first_digit = first_digit + 1;
+}
